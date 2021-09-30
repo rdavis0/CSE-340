6df212ac57a495563366e9362074f9daa4896325
rdavis0 <rdfiasco@gmail.com>
2021-09-30 16:28:46 -0600

    added fonts

--- phpmotors/home.php
+++ phpmotors/home.php
@@ -4,10 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <title>PHP Motors</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+    <title>PHP Motors | Home</title>
     <link rel="stylesheet" href="/phpmotors/css/style.css" media="screen">
 </head>
 
